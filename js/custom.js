0b0cc3ea137dc5e9c3b4c3d4c54468150c390789
freeman <iviewn@gmail.com>
2023-02-21 15:54:24 +0300

    class

--- js/custom.js
+++ js/custom.js
@@ -98,7 +98,17 @@ $(document).ready(function () {
         $('#'+activeTab).addClass('vis');
         return false;
     });
-
+    $('.modal__close').click(function() {
+        $('body').removeClass('oh');
+        $('.modal-bg').removeClass('vis');
+        $('.modal').removeClass('vis');
+    });
+    function modal(){
+        $('body').addClass('oh');
+        $('.modal-bg').addClass('vis');
+        $('.modal').addClass('vis');
+    }
+    $('.article__title').click(modal);
 });
 
 
